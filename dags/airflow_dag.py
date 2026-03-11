4c020c2c29bf78b9e179b49d358bf0d6a13098dc
priyamranade <priyamaatish1131@gmail.com>
2026-03-11 14:06:39 +0530

    Update airflow_dag.py

--- dags/airflow_dag.py
+++ dags/airflow_dag.py
@@ -12,7 +12,7 @@ dag = DAG(
 
 run_pyspark_test = BashOperator(
     task_id='run_pyspark_test',
-    bash_command='python /opt/airflow/dags/pyspark/airflow_dag_script.py',
+    bash_command='python /opt/airflow/dags/airflow_dag_script.py',
     dag=dag,
     env={
         'PYSPARK_PYTHON': 'python3',
